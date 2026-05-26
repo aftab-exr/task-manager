6d3671801640772f17c1baf2f5ab6af2d6c9e84a
Aaftab <aftabsaifi2050@gmail.com>
2026-05-26 20:49:25 +0530

    fix: update AI decompile endpoint URL to new service location

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -92,7 +92,7 @@ const aiDecompile = asyncHandler(async (req, res) => {
 
     try {
         // 1. Send the brief to your Python Microservice
-        const aiResponse = await axios.post('https://young-shirts-pick.loca.lt/api/v1/breakdown', {
+        const aiResponse = await axios.post('https://moody-goats-float.loca.lt/api/v1/breakdown', {
             mission_brief
         },{
             headers: {
